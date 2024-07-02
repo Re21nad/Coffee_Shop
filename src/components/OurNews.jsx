a62779b1fd259f2d5555e76ae5ccffb5dccccfa2
Renad <renadfilfilan3@gmail.com>
2024-07-02 22:04:00 +0300

    committed

---
+++ src/components/OurNews.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function OurNews(){
+    return(
+        <div>
+            <h1>Our Latest News</h1>
+            
+            
+        </div>
+    );
+}
